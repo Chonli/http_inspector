470b7e00da18469a0782596e87dc1b50b5948a03
Chonli <Chonli@users.noreply.github.com>
2026-01-02 12:06:00 +0100

    feat: add searchbar

--- lib/src/widgets/list/list_logs_view.dart
+++ lib/src/widgets/list/list_logs_view.dart
@@ -3,10 +3,13 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:http_inspector/src/data/network_log.dart';
 import 'package:http_inspector/src/di/inspector.dart';
+import 'package:http_inspector/src/extensions/network_log.dart';
 import 'package:http_inspector/src/widgets/list/list_log_tile_view.dart';
 
 class ListLogsView extends StatelessWidget {
   const ListLogsView({super.key});
+
+  static final ValueNotifier<String> _searchQuery = ValueNotifier<String>('');
 
   @override
   Widget build(BuildContext context) {
@@ -14,29 +17,51 @@ class ListLogsView extends StatelessWidget {
       return const Center(child: Text('Package is inactive'));
     }
 
-    return StreamBuilder<ListQueue<NetworkLog>>(
-      stream: InspectorDI.logsStream,
-      builder: (context, snapshot) {
-        final logs = snapshot.data ?? InspectorDI.logs;
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Column(
+        spacing: 12,
+        children: [
+          SearchBar(
+            leading: const Icon(Icons.search),
+            hintText: "Query...",
+            onChanged: (v) => _searchQuery.value = v,
+          ),
+          Expanded(
+            child: StreamBuilder<ListQueue<NetworkLog>>(
+              stream: InspectorDI.logsStream,
+              builder: (context, snapshot) {
+                final logs = snapshot.data ?? InspectorDI.logs;
 
-        if (snapshot.connectionState == ConnectionState.waiting &&
-            logs.isEmpty) {
-          return const Center(child: CircularProgressIndicator());
-        }
+                if (snapshot.connectionState == ConnectionState.waiting &&
+                    logs.isEmpty) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-        // TODO add filters and searchbar
+                return ValueListenableBuilder<String>(
+                  valueListenable: _searchQuery,
+                  builder: (context, query, _) {
+                    final filtered = logs.filtered(query);
 
-        if (logs.isEmpty) {
-          return const Center(child: Text('No network logs found.'));
-        }
+                    if (filtered.isEmpty) {
+                      return const Center(
+                        child: Text('Aucune requête réseau trouvée.'),
+                      );
+                    }
 
-        return ListView.builder(
-          padding: const EdgeInsets.all(12),
-          itemCount: logs.length,
-          itemBuilder: (context, index) =>
-              NetworkLogListTile(log: logs.elementAt(index)),
-        );
-      },
+                    return ListView.builder(
+                      padding: const EdgeInsets.all(0),
+                      itemCount: filtered.length,
+                      itemBuilder: (context, index) =>
+                          NetworkLogListTile(log: filtered.elementAt(index)),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
